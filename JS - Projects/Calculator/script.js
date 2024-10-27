bac13c09a44990dc8b75ee60fb104f0840dc03e9
Patryk Karlinski <karlinski96@gmail.com>
2024-10-27 20:12:44 +0100

    Code refactoring

--- JS - Projects/Calculator/script.js
+++ JS - Projects/Calculator/script.js
@@ -1,88 +1,97 @@
-const buttons = document.querySelectorAll('.button');
-const numberArea = document.querySelector('.summary');
-const clearButton = document.querySelector('.clear');
-const additionButton = document.querySelector('.addition');
-const equalButton = document.querySelector('.equal');
-const operationsButton =document.querySelectorAll('.operations')
-const deleteButton = document.querySelector('.delete')
+let buffer = '0';
+let runningTotal = 0;
+let previousOperator;
+const summary = document.querySelector('.summary');
 
-let firstNum = 0;
-let sign;
-
-buttons.forEach(button => {
-    button.addEventListener("click", function(e) {
-        if (numberArea.textContent == 0){
-            numberArea.textContent = '';
-        }
-        numberArea.textContent += e.target.innerText;
-    })
-})
-
-operationsButton.forEach(button => {
-    button.addEventListener('click', function(e) {
-        if (numberArea.textContent !== '0') {
-            firstNum = parseInt(numberArea.textContent);
-            numberArea.textContent = '0';
-            sign = e.target.innerText;
-        }
-    })
-})
-
-clearButton.addEventListener("click", function() {
-    numberArea.textContent = 0;
-    firstNum = 0;
-    sign = '';
-})
-
-deleteButton.addEventListener('click', function() {
-    if (numberArea.textContent.length > 1) {
-        numberArea.textContent = numberArea.textContent.slice(0, -1);
+function buttonClick(value) {
+    if (isNaN(parseInt(value))){
+        handleSymbol(value);
     } else {
-        numberArea.textContent = '0';
+        handleNumber(value);
     }
-})
+    rerender()
+}
 
-equalButton.addEventListener('click', function(){
-    // if(sign === '+'){
-    //     let equal = 0;
-    //     equal = firstNum + parseInt(numberArea.textContent);
-    //     numberArea.textContent = equal;
-    // }
-    // if(sign === '-'){
-    //     let equal = 0;
-    //     equal = firstNum - parseInt(numberArea.textContent);
-    //     numberArea.textContent = equal;
-    // }
-    // if(sign === '÷'){
-    //     let equal = 0;
-    //     equal = firstNum / parseInt(numberArea.textContent);
-    //     numberArea.textContent = equal;
-    // }
-    // if(sign === 'x'){
-    //     let equal = 0;
-    //     equal = firstNum * parseInt(numberArea.textContent);
-    //     numberArea.textContent = equal;
-    // }
-    
-    if(sign){
-        const secondNum = parseInt(numberArea.textContent);
-        let result;
-        switch (sign){
-            case '+' : result = firstNum + secondNum; break;
-            case '-' : result = firstNum - secondNum; break;
-            case 'x' : result = firstNum * secondNum; break;
-            case '÷':
-                if(secondNum === 0){
-                    numberArea.textContent = 'Error';
-                    return;
-                }
-                result = firstNum / secondNum;
-                break;
-        }
-        numberArea.textContent = result;
-        sign = '';
-        firstNum = 0;
+function handleNumber(number) {
+    if (buffer === '0'){
+    buffer = number;
+    } else{
+        buffer += number;
+    }
+}
+
+function handleMath(value){
+    if (buffer === '0') {
+        //do nothing
+        return;
     }
 
-   
-})
+    const intBuffer = parseInt(buffer);
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    }   else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = value;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    switch (previousOperator){
+       case '+' : runningTotal += intBuffer; break;
+       case '-' : runningTotal -= intBuffer; break;
+       case 'x' : runningTotal *= intBuffer; break;
+       case '÷':
+           if(intBuffer === 0){
+               numberArea.textContent = 'Error';
+               return;
+           }
+           runningTotal /= intBuffer;
+           break;
+}
+}
+
+function handleSymbol(symbol){
+    switch (symbol){
+        case 'C':
+            buffer = '0';
+            break;
+        case '=':
+            if(!previousOperator) {
+                return;
+            }
+            flushOperation(parseInt(buffer))
+            previousOperator = null;
+            buffer = '' + runningTotal;
+            runningTotal = 0;
+            break;
+        case '←':
+            if (buffer.length === 1){
+                buffer = '0';
+            } else {
+                buffer = buffer.substring(0, buffer.length -1);
+            }
+            break;
+        case '+':
+        case '-':
+        case '÷':
+        case 'x':
+            handleMath(symbol);
+            break;
+    }
+}
+
+function init() {
+    document
+        .querySelector('.buttons')
+        .addEventListener('click', function(e) {
+            buttonClick(e.target.innerText)
+        })
+}
+
+function rerender() {
+    summary.innerText = buffer;
+}
+
+init();
